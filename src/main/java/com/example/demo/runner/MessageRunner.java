4f041ba57e3eac1da56865dce3249f3c622294e6
ARJUN RAJ <ARJUN RAJ@DESKTOP-SL0E0N8>
2021-01-27 19:25:50 +0530

    WH-998 Commit

--- src/main/java/com/example/demo/runner/MessageRunner.java
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -1,15 +1,20 @@
 package com.example.demo.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRunner implements CommandLineRunner {
 
+	
+	private static Logger log = LoggerFactory.getLogger(MessageRunner.class);
+	
 	@Override
 	public void run(String... args) throws Exception {
 		int a =10;
 		System.out.println("Modified Code"+a);
-	}
+	} 
 
 }
